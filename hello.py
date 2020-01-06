9a21e5e96e6c6d15e535d8524261a35004c33780
eXetrum <shishimorovmaksim@gmail.com>
2020-01-06 10:27:52 +0200

    fix wsgi body

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     headers = [ ('Content-Type', 'text/plain')]
     query = environ['QUERY_STRING']
-    bidy = "\n".join(query.split("&"))
+    body = "\n".join(query.split("&"))
     start_response(status, headers )
     return [ body ]
