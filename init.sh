4cb56de1d59f2e31a8a1d4a2c5b0196080f7d76c
eXetrum <shishimorovmaksim@gmail.com>
2020-01-06 09:45:13 +0200

    bash

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn stop
-sudo /etc/init.d/gunicorn -c hello.py hello:app
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c hello.py hello:app
 ﻿sudo /etc/init.d/mysql start﻿
