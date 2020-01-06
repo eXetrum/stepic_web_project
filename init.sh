13f38b6c7e1c1bf358d6161776a3f2c85d32d2f6
eXetrum <shishimorovmaksim@gmail.com>
2020-01-06 09:50:50 +0200

    fix init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,4 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c hello.py hello:wsgi_app
 ﻿sudo /etc/init.d/mysql start﻿
