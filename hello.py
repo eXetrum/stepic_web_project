778644c4c580a1738910871bb8c31fb7fac7f607
eXetrum <shishimorovmaksim@gmail.com>
2020-01-06 10:25:01 +0200

    hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@
 def wsgi_app(environ, start_response):
     status = '200 OK'
     headers = [ ('Content-Type', 'text/plain')]
-    body = 'Hello, world!'
+    body = environ['QUERY_STRING']
     start_response(status, headers )
     return [ body ]
