a37c3e7f938711b9cedb1f5b17d6bc0abd0850b2
eXetrum <shishimorovmaksim@gmail.com>
2022-07-08 18:31:52 +0300

    'ask' cfg
    Added upstreams (nginx)
    init.sh refactoring

--- init.sh
+++ init.sh
@@ -1,8 +1,21 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+#!/bin/bash
+
+WORK_DIR="/home/box"
+PROJ_DIR="$WORK_DIR/web"
+NGINX_DIR="/etc/nginx"
+GUNICORN_DIR="/etc/gunicorn.d"
+
+# nginx
+sudo ln -sf $PROJ_DIR/etc/nginx.conf $NGINX_DIR/sites-enabled/nginx.conf
+sudo rm -rf $NGINX_DIR/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/wsgi.example
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/test-django
+
+# gunicorn
+sudo rm -f $GUNICORN_DIR*
+sudo ln -sf $PROJ_DIR/etc/hello.conf	$GUNICORN_DIR/hello.conf
+sudo ln -sf $PROJ_DIR/etc/ask.conf		$GUNICORN_DIR/ask.conf
 sudo service gunicorn restart
 sudo /etc/init.d/gunicorn restart
+
+# MySQL
 ﻿sudo /etc/init.d/mysql start﻿
