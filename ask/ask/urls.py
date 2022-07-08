d9477cd34c83cf8f7df35e38ec8deba6029c69e3
eXetrum <shishimorovmaksim@gmail.com>
2022-07-08 20:42:33 +0300

    cccommma

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^$', include('qa.urls')),,
+    url(r'^$', include('qa.urls')),
     url(r'^/login/', include('qa.urls')),
     url(r'^/signup/', include('qa.urls')),
     url(r'^/question/(\d+)/$', include('qa.urls')),
