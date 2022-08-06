5f2c1e36d0040751c97663e86f52293775b11668
eXetrum <shishimorovmaksim@gmail.com>
2022-08-06 16:07:40 +0300

    Update init.sh
    init mysql

--- init.sh
+++ init.sh
@@ -5,6 +5,12 @@ PROJ_DIR="$WORK_DIR/web"
 NGINX_DIR="/etc/nginx"
 GUNICORN_DIR="/etc/gunicorn.d"
 
+DB_HOSTNAME="localhost"
+DB_NAME="stepic_web_project"
+DB_USER="box"
+DB_PASSWORD=""
+
+
 # nginx
 sudo ln -sf $PROJ_DIR/etc/nginx.conf $NGINX_DIR/sites-enabled/nginx.conf
 sudo rm -rf $NGINX_DIR/sites-enabled/default
@@ -19,3 +25,7 @@ sudo /etc/init.d/gunicorn restart
 
 #MySQL
 sudo /etc/init.d/mysql start
+sudo mysql -uroot -e "create database $DB_NAME"
+sudo mysql -uroot -e "grant all privileges on $DB_NAME.* to '$DB_USER'@'$DB_HOST' with grant option;"
+sudo ~/web/ask/manage.py makemigrations
+sudo ~/web/ask/manage.py migrate
