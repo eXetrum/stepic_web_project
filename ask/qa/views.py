ba89b2f64f9cc526d925adbf462a7320e8207a67
eXetrum <shishimorovmaksim@gmail.com>
2022-08-11 18:08:32 +0300

    route

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -66,9 +66,8 @@ def popular(request):
 def question(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
+        return render(request, 'question.html', { 
+            'question': question_id,
+        })
     except Question.DoesNotExist:
-        raise Http404
-    
-    return render(request, 'question.html', { 
-        'question': question,
-    })    
+        raise Http404    
