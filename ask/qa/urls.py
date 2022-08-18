c1c8376b144f158fd8651dfb7e0c8e3ee638bb61
eXetrum <shishimorovmaksim@gmail.com>
2022-08-18 05:30:57 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,25 +2,14 @@
 from django.conf.urls import url, include
 from . import views
 
-"""
-urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^popular/$', views.popular),
-    url(r'^question/(?P<id>\d+)/$', views.show_question),
-    url(r'^ask/$', views.create_question),    
-    url(r'^login/$', views.test),
-    url(r'^signup/$', views.test),
-    url(r'^new/$', views.test),
-]
-"""
 
 urlpatterns = [
-    url(r''$, views.home, name='home'),
-    url(r'popular/'$, views.popular, name='popular'),
-    url(r'question/(?P<id>\d+)/'$, views.show_question, name='question'),
-    url(r'ask/'$, views.create_question, name='ask'),
-    url(r'login/'$, views.login_page, name='login'),
-    url(r'signup/'$, views.signup_page, name='signup'),
-    url(r'logout/'$, views.logout_page, name='logout'),
-    url(r'new/'$, views.test),
+    url(r'^$', views.home, name='home'),
+    url(r'^popular/$', views.popular, name='popular'),
+    url(r'^question/(?P<id>\d+)/$', views.show_question, name='question'),
+    url(r'^ask/$', views.create_question, name='ask'),
+    url(r'^login/$', views.login_page, name='login'),
+    url(r'^signup/$', views.signup_page, name='signup'),
+    url(r'^logout/$', views.logout_page, name='logout'),
+    url(r'^new/$', views.test),
 ]
