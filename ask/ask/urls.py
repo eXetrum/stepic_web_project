8220cd2c859b3e2c48996f8db1c324bbdd0b86b3
eXetrum <shishimorovmaksim@gmail.com>
2022-08-18 05:39:04 +0300

    url => path

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,5 +18,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
                                                                                 
 urlpatterns = [
-    url('', include('qa.urls')),
+    #path('', include('qa.urls')),
+    url(r'^$', include('qa.urls')),
 ]
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -13,3 +13,16 @@ urlpatterns = [
     url(r'^logout/$', views.logout_page, name='logout'),
     url(r'^new/$', views.test),
 ]
+"""
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('popular/', views.popular, name='popular'),
+    path('question/<int:id>/', views.show_question, name='question'),
+    path('ask/', views.create_question, name='ask'),
+    path('login/', views.login_page, name='login'),
+    path('signup/', views.signup_page, name='signup'),
+    path('logout/', views.logout_page, name='logout'),
+    path('new/', views.test),
+]
+"""
