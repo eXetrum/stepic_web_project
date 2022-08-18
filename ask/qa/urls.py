1b2790d08fa282bb6e4a8269ba02b2887b1552d2
eXetrum <shishimorovmaksim@gmail.com>
2022-08-18 05:28:27 +0300

    backpedal django+python version

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,13 +1,26 @@
-from django.urls import include, path
+#from django.urls import include, path
+from django.conf.urls import url, include
 from . import views
 
+"""
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('popular/', views.popular, name='popular'),
-    path('question/<int:id>/', views.show_question, name='question'),
-    path('ask/', views.create_question, name='ask'),
-    path('login/', views.login_page, name='login'),
-    path('signup/', views.signup_page, name='signup'),
-    path('logout/', views.logout_page, name='logout'),
-    path('new/', views.test),
+    url(r'^$', views.home),
+    url(r'^popular/$', views.popular),
+    url(r'^question/(?P<id>\d+)/$', views.show_question),
+    url(r'^ask/$', views.create_question),    
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^new/$', views.test),
+]
+"""
+
+urlpatterns = [
+    url(r''$, views.home, name='home'),
+    url(r'popular/'$, views.popular, name='popular'),
+    url(r'question/(?P<id>\d+)/'$, views.show_question, name='question'),
+    url(r'ask/'$, views.create_question, name='ask'),
+    url(r'login/'$, views.login_page, name='login'),
+    url(r'signup/'$, views.signup_page, name='signup'),
+    url(r'logout/'$, views.logout_page, name='logout'),
+    url(r'new/'$, views.test),
 ]
